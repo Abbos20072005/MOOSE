3633bbb9ae8543218e8578a9ce8b487257dcfb12
abbos <hikmatullaevabbos24@gmail.com>
2024-07-04 20:59:24 +0500

    gunicorn added

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "143.244.134.2:8011"  # Replace with your desired IP and port
+bind = "143.244.134.2:8013"  # Replace with your desired IP and port
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread"
 threads = 2
